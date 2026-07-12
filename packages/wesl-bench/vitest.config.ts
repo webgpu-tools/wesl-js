641c29c1253b8de440337844a1aba767cac779a7
lee mighdoll <lee@underneath.ca>
2026-07-11 23:16:50 -0700

    (wesl-bench) clean out old tests

--- packages/wesl-bench/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globals: false,
-    environment: "node",
-    exclude: ["**/*.integration.ts", "node_modules/**"],
-  },
-});
