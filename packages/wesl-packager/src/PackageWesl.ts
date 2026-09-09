db2de02fb512028651d9ac4151d32e641d81873e
Konasov Dmitriy <d.konasov@yandex.ru>
2026-09-09 20:05:55 +0100

    (wesl-packager) scoped packages fix for bundling

--- packages/wesl-packager/src/PackageWesl.ts
+++ packages/wesl-packager/src/PackageWesl.ts
@@ -136,7 +136,9 @@ async function writeJsBundle(
 ): Promise<void> {
   await mkdir(outDir, { recursive: true });
 
-  const depNames = dependencies.map(dep => dep.replaceAll("/", "_"));
+  const depNames = dependencies.map(dep =>
+    dep.replaceAll("/", "_").replaceAll("@", ""),
+  );
   const depsWithNames = zip(dependencies, depNames);
 
   const imports = depsWithNames
